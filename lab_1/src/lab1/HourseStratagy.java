3f031c290c204dd567f39f57510f9ec8bd1b67a7
cherkasoov <cherkasov5509@gmail.com>
2025-11-18 21:50:01 +0800

    Initial first commit

---
+++ lab_1/src/lab1/HourseStratagy.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class HourseStratagy implements MoveStratagy {
+    @Override
+    public void move(){
+        System.out.println("hoursing!");
+    }
+}
---
+++ lab_1/src/lab1/WalkStratagy.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class WalkStratagy implements MoveStratagy {
+    @Override
+    public void move() {
+        System.out.println("walking!");
+    }
+}
